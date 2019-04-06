43e5384cabd3cfce3b7ec84f030e66c4da81b9f9
chenlong <1583479514@qq.com>
2019-04-06 08:53:35 +0800

    拖拽基本功能实现

--- src/page/teachers/course/groupClass/index.vue
+++ src/page/teachers/course/groupClass/index.vue
@@ -17,29 +17,59 @@
     </div>
 
     <div class="group-content">
-      <div class="swiper-container">
-        <div class="swiper-wrapper">
-          <draggable class="list-group" :list="list1" group="people" @change="log">
-            <div
-              class="list-group-item"
-              v-for="element in list1"
-              :key="element.id"
-              style="width: 100px; height: 10px; background: green;"
-            >{{ element.name }}</div>
-          </draggable>
+      <div class="group1-wrap">
+        <div class="content-1">
+          <div class="content-1-wrap">
+            <draggable class="list-group" :list="list1" group="people" @change="log">
+              <div class="list-group-item" v-for="(element, index) in list1" :key="element.name">
+                <img class="avatar-img" src="../../../../assets/images/logo.png" alt>
+                <div class="avatar-name">欧阳娜娜</div>
+              </div>
+            </draggable>
+          </div>
+        </div>
+        <div class="group1-prev">
+          <i class="el-icon-arrow-left"></i>
+        </div>
+        <div class="group1-next">
+          <i class="el-icon-arrow-right"></i>
         </div>
       </div>
-      <div class="swiper-container">
-        <div class="swiper-wrapper">
-          <draggable class="list-group" :list="list1" group="people" @change="log">
-            <div
-              class="list-group-item"
-              v-for="element in list2"
-              :key="element.id"
-            >{{ element.name }}</div>
-          </draggable>
+
+      <div class="group1-wrap group2-wrap">
+        <div class="content-1">
+          <div class="content-1-wrap">
+            <div class="group-slide" v-for="item in  10" :key="item">
+              <div class="group-slide-innder">
+                <div class="group-title">09级2班 01组</div>
+                <div class="group2-inner">
+                  <draggable
+                    class="list-group list-group2"
+                    :list="list2"
+                    group="people"
+                    @change="log"
+                  >
+                    <div class="avatar-list" v-for="(element, index) in list2" :key="element.name">
+                      <img class="avatar-img" src="../../../../assets/images/logo.png" alt>
+                      <div class="avatar-name">欧阳娜娜</div>
+                    </div>
+                  </draggable>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="group1-prev">
+          <i class="el-icon-arrow-left"></i>
+        </div>
+        <div class="group1-next">
+          <i class="el-icon-arrow-right"></i>
         </div>
       </div>
+    </div>
+    <div class="group-btn">
+      <div class="submit base-btn">提交</div>
+      <div class="reset base-btn">重置</div>
     </div>
   </div>
 </template>
@@ -47,11 +77,7 @@
 <script>
 /*组件方式引用*/
 import draggable from "vuedraggable";
-let idGlobal = 8;
 export default {
-  display: "Clone on Control",
-  instruction: "Press Ctrl to clone element from list 1",
-  order: 4,
   props: {
     showLesson: {
       type: Boolean,
@@ -80,17 +106,34 @@ export default {
         content: ""
       },
       list1: [
-        { name: "John", id: 1 },
+        { name: "John1", id: 1 },
         { name: "Joao", id: 2 },
         { name: "Jean", id: 3 },
-        { name: "Gerard", id: 4 }
+        { name: "Gerard", id: 4 },
+        { name: "Juan1", id: 5 },
+        { name: "Edgard", id: 6 },
+        { name: "Johnson", id: 7 },
+        { name: "Juan", id: 8 },
+        { name: "Edgard1", id: 9 },
+        { name: "Johnson0", id: 10 }
       ],
       list2: [
-        { name: "Juan", id: 5 },
-        { name: "Edgard", id: 6 },
-        { name: "Johnson", id: 7 }
+        { name: "Juan2", id: 11 },
+        { name: "Edgard2", id: 12 },
+        { name: "Johnson1", id: 13 },
+        { name: "Johnson2", id: 14 },
+        { name: "Johnson3", id: 15 },
+        { name: "Johnson4", id: 16 },
+        { name: "Johnson5", id: 17 },
+        { name: "Johnson6", id: 18 },
+        { name: "Johnson7", id: 19 },
+        { name: "Johnson8", id: 20 }
       ],
-      controlOnStart: true
+      list3: [
+        { name: "Juan3", id: 8 },
+        { name: "Edgard3", id: 9 },
+        { name: "Johnson9", id: 10 }
+      ]
     };
   },
   watch: {
@@ -101,14 +144,19 @@ export default {
   },
   created() {},
   methods: {
-    clone({ name }) {
-      return { name, id: idGlobal++ };
+    add: function() {
+      this.list.push({ name: "Juan" });
     },
-    pullFunction() {
-      return this.controlOnStart ? "clone" : true;
+    replace: function() {
+      this.list = [{ name: "Edgard" }];
     },
-    start({ originalEvent }) {
-      this.controlOnStart = originalEvent.ctrlKey;
+    clone: function(el) {
+      return {
+        name: el.name + " cloned"
+      };
+    },
+    log: function(evt) {
+      window.console.log(evt);
     },
     closeModal() {
       this.$emit("closeModal", false);
@@ -124,7 +172,7 @@ export default {
 <style lang="scss" scoped>
 .group {
   width: 1100px;
-  height: 520px;
+  height: 620px;
 
   .group-header-wrap {
     height: 60px;
@@ -200,6 +248,145 @@ export default {
     transform: translateY(-50%);
     right: 0;
   }
+}
+
+.group-content {
+  padding-top: 20px;
+}
+
+.content-1 {
+  width: 954px;
+  margin: 0 auto;
+  overflow-x: scroll;
+
+  .content-1-wrap {
+    width: 100%;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+    box-sizing: border-box;
+  }
+}
+
+.list-group-item,
+.group-slide {
+  width: 126px;
+  height: 146px;
+  display: inline-block;
+  text-align: center;
+  vertical-align: middle;
+  background: #fff;
+  border: 1px solid #e4e8ed;
+  margin-right: 12px;
+  box-sizing: border-box;
+}
+
+.group1-wrap {
+  width: 100%;
+  position: relative;
+}
+
+.group2-wrap {
+  height: 265px;
+}
+
+.group1-prev,
+.group1-next {
+  width: 34px;
+  height: 34px;
+  background: #eee;
+  border-radius: 34px;
+  text-align: center;
+  line-height: 34px;
+  font-size: 18px;
+  color: #fff;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.group1-prev {
+  left: 20px;
+}
+
+.group1-next {
+  right: 20px;
+}
+
+.group-slide {
+  width: 310px;
+  height: 265px;
+  background: #fafbfd;
+  margin-right: 12px;
+  margin-top: 20px;
+}
+
+.group-title {
+  height: 50px;
+  line-height: 50px;
+  font-size: 16px;
+  font-weight: bold;
+  background: rgba(255, 243, 229, 1);
+}
+
+.group-list {
+  height: 215px;
+}
+
+.avatar-list {
+  display: inline-block;
+  width: 25%;
+  text-align: center;
+}
+
+.avatar-img {
+  width: 50px;
+  height: 50px;
+  margin-bottom: 5px;
+}
+
+.avatar-name {
+  font-size: 14px;
+  font-weight: bold;
+  color: rgba(51, 51, 51, 1);
+  line-height: 26px;
+}
+
+.group2-inner {
+  height: 215px;
+}
+
+.list-group2 {
+  overflow-y: scroll;
+  height: 215px;
+  text-align: left;
+  white-space: pre-wrap;
+}
+
+.group-btn {
+  text-align: center;
+  margin-top: 50px;
+}
+
+.base-btn {
+  display: inline-block;
+  vertical-align: middle;
+  width: 150px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  border-radius: 20px;
+}
+
+.submit {
+  background: rgba(247, 151, 39, 1);
+  color: #fff;
+  margin-right: 20px;
+}
+
+.reset {
+  border: 1px solid #999;
+  color: #ccc;
 }
 </style>
 
