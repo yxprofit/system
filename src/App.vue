3af96ac6164bb8bf96941eaa52a4ff709d6d9916
董海南 <hainan_dong@crfchina.com>
2019-04-08 16:26:13 +0800

    适配

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,4 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   height: 100vh;
 }
-@media screen and (min-width: 1280px) {
-  #app {
-    font-size: 93.7%;
-  }
-}
-@media screen and (min-width: 1366px) {
-  #app {
-    font-size: 100%;
-  }
-}
 </style>
