1685f6e94c15c9965ca6dacda8b04a07baf78e64
董海南 <hainan_dong@crfchina.com>
2019-04-08 15:11:53 +0800

    适配1280

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,26 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'microsoft yahei', 'PingFangSC-Regular','Avenir', Helvetica, Arial, sans-serif;
+  font-family: "microsoft yahei", "PingFangSC-Regular", "Avenir", Helvetica,
+    Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100vh;
+}
+@media screen and (min-width: 1280px) {
+  #app {
+    font-size: 93.7%;
+  }
+}
+@media screen and (min-width: 1366px) {
+  #app {
+    font-size: 100%;
+  }
 }
 </style>
